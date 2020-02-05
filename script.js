969c0f04a9b302f6d6b1ebdf2dc3db4f14d81c91
Michael Clare <michaelclare@michaels-mbp.home>
2020-02-05 18:34:31 +0000

    started JS

---
+++ script.js
@@ -0,0 +1,111 @@
+const words = ["RACE"]
+
+let a = document.getElementsByClassName("button")[0]
+let b = document.getElementsByClassName("button")[1]
+let c = document.getElementsByClassName("button")[2]
+let d = document.getElementsByClassName("button")[3]
+let e = document.getElementsByClassName("button")[4]
+let f = document.getElementsByClassName("button")[5]
+let g = document.getElementsByClassName("button")[6]
+let h = document.getElementsByClassName("button")[7]
+let i = document.getElementsByClassName("button")[8]
+let j = document.getElementsByClassName("button")[9]
+let k = document.getElementsByClassName("button")[10]
+let l = document.getElementsByClassName("button")[11]
+let m = document.getElementsByClassName("button")[12]
+let n = document.getElementsByClassName("button")[13]
+let o = document.getElementsByClassName("button")[14]
+let p = document.getElementsByClassName("button")[15]
+let q = document.getElementsByClassName("button")[16]
+let r = document.getElementsByClassName("button")[17]
+let s = document.getElementsByClassName("button")[18]
+let t = document.getElementsByClassName("button")[19]
+let u = document.getElementsByClassName("button")[20]
+let v = document.getElementsByClassName("button")[21]
+let w = document.getElementsByClassName("button")[22]
+let x = document.getElementsByClassName("button")[23]
+let y = document.getElementsByClassName("button")[24]
+let z = document.getElementsByClassName("button")[25]
+
+document.addEventListener("click",(event)=> {
+    switch (true) {
+        case event.target == a:
+            console.log("A")
+            break
+        case event.target == b:
+            console.log("B")
+            break
+        case event.target == c:
+            console.log("C")
+            break
+        case event.target == d:
+            console.log("D")
+            break
+        case event.target == e:
+            console.log("E")
+            break
+        case event.target == f:
+            console.log("F")
+            break
+        case event.target == g:
+            console.log("G")
+            break
+        case event.target == h:
+            console.log("H")
+            break
+        case event.target == i:
+            console.log("I")
+            break
+        case event.target == j:
+            console.log("J")
+            break
+        case event.target == k:
+            console.log("K")
+            break
+        case event.target == l:
+            console.log("L")
+            break
+        case event.target == m:
+            console.log("M")
+            break
+        case event.target == n:
+            console.log("N")
+            break
+        case event.target == o:
+            console.log("O")
+            break
+        case event.target == p:
+            console.log("P")
+            break
+        case event.target == q:
+            console.log("Q")
+            break
+        case event.target == r:
+            console.log("R")
+            break
+        case event.target == s:
+            console.log("S")
+            break
+        case event.target == t:
+            console.log("T")
+            break
+        case event.target == u:
+            console.log("U")
+            break
+        case event.target == v:
+            console.log("V")
+            break
+        case event.target == w:
+            console.log("W")
+            break
+        case event.target == x:
+            console.log("X")
+            break
+        case event.target == y:
+            console.log("Y")
+            break
+        case event.target == z:
+            console.log("Z")
+            break
+    }
+})
